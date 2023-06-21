4cbb625a2b358fcf06698d01c97825ef320a8def
meirshoulkin <meirshoulkin@gmail.com>
2023-06-21 15:35:49 +0300

    HW_8_last

--- src/test/java/tests/LoginTest.java
+++ src/test/java/tests/LoginTest.java
@@ -1,5 +1,8 @@
 package tests;
 
+import models.User;
+import org.openqa.selenium.By;
+import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -23,9 +26,20 @@ public class LoginTest extends TestBase{
         // fill login form
         app.getUser().fillLoginRegistrationForm(email, password);
         app.getUser().submitLogin();
-//        Assert.assertTrue(wd.findElements(By.xpath("//*[text()='Sign Out']")).size() > 0);
+    //    Assert.assertTrue(wd.findElements(By.xpath("//*[text()='Sign Out']")).size() > 0);
         app.getUser().pause(3000);
-//        Assert.assertTrue(isElementPresent(By.xpath("//*[text()='Sign Out']")));
+  //      Assert.assertTrue(isElementPresent(By.xpath("//*[text()='Sign Out']")));
+   }    @Test
+    public void loginPositiveTestUser(){
+        User user = new User().withEmail("abc@def.com").withPassword("$Abcdef12345");
+        // open login form
+        app.getUser().openLoginRegistrationForm();
+        // fill login form
+       // app.getUser().fillLoginRegistrationForm(user.getEmail(), user.getPassword());
+        app.getUser().fillLoginRegistrationForm(user);
+        app.getUser().submitLogin();
+        app.getUser().pause(3000);
+       Assert.assertTrue(app.getUser().isElementPresent(By.xpath("//*[text()='Sign Out']")));
    }
 
     @Test
